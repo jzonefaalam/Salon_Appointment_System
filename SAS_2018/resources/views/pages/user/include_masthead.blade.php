60d86d62ab4914148181623cd1abde046c668405
vincent <pepinocarlangelo@yahoo.com>
2018-04-01 16:37:49 +0800

    Changes on Carousel

--- SAS_2018/resources/views/pages/user/include_masthead.blade.php
+++ SAS_2018/resources/views/pages/user/include_masthead.blade.php
@@ -4,7 +4,7 @@
             <div class="container">
                 <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
                     <div class="carousel-inner" role="listbox" style="height: 450px; border-radius: 10px;">
-                        <div class="carousel-item active" style="background-image: url('/images/carousel_1.jpg')">
+                        <div class="carousel-item active" style="background-image: url('/images/cr1.jpg')">
                         </div>
                         <div class="carousel-item" style="background-image: url('/images/carousel_2.jpg')">
                         </div>
